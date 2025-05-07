eec07a6d58ae520d0867e571f25c95e0dfd791e2
Junior Avalos <junior.trey.avalos13@gmail.com>
2025-05-06 23:14:53 -0500

    added my first function with some notes for what happens when a button is clicked in the calculator

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelector('.buttons button');
 
+buttons.forEach(button =>{ // a method that can be used on a NodeList to iterate over each item in the list.
+    button.addEventListener('click', ()=>{ // a method that attaches an event listener to a specific element - in this we added 'click' followed by an arrow function.
+        const buttonText = button.textContent;
+        display.textContent += buttonText;
+    });
+});
+
